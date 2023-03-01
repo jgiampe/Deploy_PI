6d9af8d252866b97dde825168f8223c45fe13aab
Julian <jggiampe@gmail.com>
2023-03-01 19:23:32 -0300

    ADDING BACKEND CHANGES

--- api/index.js
+++ api/index.js
@@ -20,6 +20,8 @@
 const server = require('./src/app.js');
 const { conn, Diet } = require('./src/db.js');
 
+require('dotenv').config();
+
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
 
@@ -39,7 +41,7 @@ conn.sync({ force: true }).then(async () => {
     {name: 'omnivore'},
    ])
    
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
